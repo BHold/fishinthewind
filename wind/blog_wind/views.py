1fc24569731afa45d0de47401796fdebdac13d21
Brian Holdefehr <bhold45@gmail.com>
2012-07-14 19:43:55 -0400

    added basic views, urls, and began work on html/css

--- wind/blog_wind/views.py
+++ wind/blog_wind/views.py
@@ -1,1 +1,66 @@
-# Create your views here.
+from django.shortcuts import get_object_or_404, render_to_response
+from django.template import RequestContext
+from django.core.paginator import Paginator, InvalidPage, EmptyPage
+
+from blog_wind.models import Post
+
+def home(request, page=1):
+    """
+    Home page for blog
+
+    Will display most recents posts regardless of
+    if they are essays or galleries
+    """
+
+    all_posts = Post.objects.get_active()
+
+    paginator = Paginator(all_posts, 7)
+
+    # if page number out of range, give last page
+    try: 
+        posts = paginator.page(page)
+    except (EmptyPage, InvalidPage):
+        posts = paginator.page(paginator.num_pages)
+
+    variables = RequestContext(request, {
+        'posts': posts
+    })
+    return render_to_response('home.html', variables)
+
+
+
+def post(request, slug):
+    """
+    Displays an individual post
+    """
+
+    post = get_object_or_404(Post, slug=slug)
+
+    variables = RequestContext(request, {
+        'post': post 
+    })
+    return render_to_response('post.html', variables)
+
+def essays(request):
+    """
+    A page that will list all of the posts that are essays only
+    """ 
+
+    essays = Post.objects.get_active().filter(is_gallery=False)
+
+    variables = RequestContext(request, {
+        'essays': essays
+    })
+    return render_to_response('essays.html', variables)
+
+def galleries(request):
+    """
+    A page that will list all of the posts that are galleries only
+    """
+
+    galleries = Post.objects.get_active().filter(is_gallery=True)
+
+    variables = RequestContext(request, {
+        'galleries': galleries 
+    })
+    return render_to_response('galleries.html', variables)
--- wind/wind/urls.py
+++ wind/wind/urls.py
@@ -4,9 +4,12 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'wind.views.home', name='home'),
-    # url(r'^wind/', include('wind.foo.urls')),
+    # Blog 
+    url(r'^$', 'blog_wind.views.home', name='home'),
+    url(r'^page/(?P<page>\d+)$', 'blog_wind.views.home', name='paged_home'),
+    url(r'^essays$', 'blog_wind.views.essays', name='essays'),
+    url(r'^photos$', 'blog_wind.views.galleries', name='galleries'),
+    url(r'^(?P<slug>[-\w]+)$', 'blog_wind.views.post', name='post'),
 
     # Admin
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
