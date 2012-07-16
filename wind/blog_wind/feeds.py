2e9211b9dbcbf2c5f33b26ce7d9decb9f00bbe0b
Brian Holdefehr <bhold45@gmail.com>
2012-07-15 23:49:32 -0400

    lots of design, added some little icons for twitter/rss, added a feed

---
+++ wind/blog_wind/feeds.py
@@ -0,0 +1,17 @@
+from django.contrib.syndication.views import Feed
+
+from blog_wind.models import Post
+
+class RecentFeed(Feed):
+    title = "Fish in the Wind by Brian Holdefehr"
+    link = "/feeds/recent"
+    description = "Recent posts on Fish in the Wind by Brian Holdefehr"
+
+    def items(self):
+        return Post.objects.get_active()[:10]
+
+    def item_title(self, item):
+        return item.title
+
+    def item_description(self, item):
+        return item.body[:150] + "..."
