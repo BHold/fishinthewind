ff0f0dd7642abdad82cc16842da2b85c47cba76b
Brian Holdefehr <bhold45@gmail.com>
2013-01-18 10:54:44 -0500

    changed filename

---
+++ wind/wind/settings/production.py
@@ -0,0 +1,27 @@
+from .base import *
+
+ADMINS = (
+    ('Brian Holdefehr', 'bhold45@gmail.com'),
+)
+
+MANAGERS = ADMINS
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'bhold',
+        'USER': 'bhold',
+        'PASSWORD': os.environ['DB_PASS'],
+        'HOST': '',
+        'PORT': '',
+    }
+}
+
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.memcached.MemcachedCache',
+        'LOCATION': 'unix:/home/bhold/memcached.sock'
+    }
+}
+
+MEDIA_URL = STATIC_URL = 'http://d3ry2ziyu0wa1k.cloudfront.net/'
